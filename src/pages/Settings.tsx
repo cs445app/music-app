abb964c0f89b8eadcf40dc0aa740b3188cb3232a
Logan Gray <lgray@proton.me>
2024-03-12 11:29:55 -0700

    Import & reset button

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,21 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonItem, IonGrid, IonRow, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Settings.css';
 
 const Settings: React.FC = () => {
+
+  // This is a placeholder function for the import button
+  const handleImport = () => {
+    console.log('Import');
+  }
+
+  // This is a placeholder function for the reset button
+  const handleReset = () => {
+    console.log('Reset');
+  }
+
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader translucent={true}>
         <IonToolbar>
           <IonTitle>Settings</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen={true} className="ion-padding">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Settings</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonList>
+          <IonGrid>
+            <IonRow>
+              <IonButton onClick={handleImport}>Import</IonButton>
+              <IonButton onClick={handleReset}>Reset</IonButton>
+            </IonRow>
+          </IonGrid>
+        </IonList>
         <ExploreContainer name="Settings page" />
       </IonContent>
     </IonPage>
